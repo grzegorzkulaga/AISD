948882a0a3b0a277fc8296863f3ac6e079fb4f89
Grzegorz Kułaga <grzegorz.kulaga@interia.pl>
2020-07-15 00:28:37 +0200

    ABCDEF

---
+++ ABCDEF/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace abcdef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] S = new int[100];
+            int output = 0;
+            int N = int.Parse(Console.ReadLine());
+            int i, j, k;
+
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (i = -30000; i <= 30000; i++)
+            {
+                dic[i] = 0;
+            }
+
+            for (i = 0; i != N; i++)
+            {
+                S[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (i = 0; i != N; i++)
+            {
+                for (j = 0; j != N; j++)
+                {
+                    for (k = 0; k != N; k++)
+                    {
+                        dic[S[i] * S[j] + S[k]]++;
+                    }
+                }
+            }
+
+            for (i = 0; i != N; i++)
+            {
+                if (S[i] == 0)
+                    continue;
+                else
+                {
+                    for (j = 0; j != N; j++)
+                    {
+                        for (k = 0; k != N; k++)
+                        {
+                            output += dic[(S[j] + S[k]) * S[i]];
+                        }
+                    }
+                }
+            }
+            Console.Write(output);
+        }
+    }
+}
---
+++ ABCDEF/README.md
@@ -0,0 +1,15 @@
+Input
+The first line contains integer N (1 ≤ N ≤ 100), the size of a set S.
+
+Elements of S are given in the next N lines, one integer per line. Given numbers will be distinct.
+
+Output
+Output the total number of plausible sextuples.
+
+Examples
+Input:
+1
+1
+
+Output:
+1
